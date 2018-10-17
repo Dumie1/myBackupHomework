ad6f3da4cc493721ce56d128fe253ca7249e6df9
Dumie1 <37332966+Dumie1@users.noreply.github.com>
2018-10-17 17:27:00 +0200

    Update App.js

--- ReactWk1b/App.js
+++ ReactWk1b/App.js
@@ -1,32 +1,22 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import Description from './Description';
+import Category from './Category';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" />
-          {/* <p>
-            Edit <code>src/App.js</code> and save to reload.
+          
+          <p>
+            Todo<span className='designLogo'>App</span>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {/* LReactearn 
-          </a>  */} 
-          <h3>Dumie's Todo List</h3>
+          
         </header>
-      
+
+        <Category />
+        <hr />
         
-        <Description />
-        <hr/>
-    
       </div>
     );
   }
