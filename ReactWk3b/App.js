19849cb2e6fe778413672efd99aa2d24f66b1042
Dumie1 <37332966+Dumie1@users.noreply.github.com>
2018-11-13 09:40:41 +0100

    Update App.js

--- ReactWk3b/App.js
+++ ReactWk3b/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import Data from './Data';
-import Header from './Header';
-import Calender from './Calender'
+import Data from './Components/Data';
+import Header from './Components/Header';
+import Calender from './Components/Calender'
 
 class App extends Component {
   render() {
-    return<div className='App'>
-        <div>
-          <Header />
-          <Data />
-          <Calender />
-        </div>
+    return <div className='App'>
+      <div>
         
+        <Header />
+        <Data />
+        <Calender />
+      </div>
+
     </div>
-    
   }
 }
 
 export default App;
+
