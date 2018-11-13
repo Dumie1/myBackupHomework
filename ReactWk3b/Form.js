c8c4502306a6faf40244fad2e493075771984073
Dumie1 <37332966+Dumie1@users.noreply.github.com>
2018-11-13 09:49:52 +0100

    Update Form.js

--- ReactWk3b/Form.js
+++ ReactWk3b/Form.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
-class Form extends React.Component{
+class Form extends React.Component {
     render() {
 
-        const { addTodo , descriptionInput , deadlineInput} = this.props; 
+        const { addTodo, descriptionInput, deadlineInput } = this.props;
 
         return (
             <form onSubmit={addTodo}>
                 <label>
-                    Description :
+                    Enter Description :
                     <input ref={descriptionInput} type='text' name='description' placeholder='Type a description...' />
                 </label>
                 <label>
                     Deadline :
                     <input ref={deadlineInput} type='date' name='deadline' />
                 </label>
-                    <input type='submit' value='Submit' className='add'/>
+                <input type='submit' value='Add' className='add' />
             </form>
 
-                )
-            }
+        )
+    }
 }
 
 export default Form;
